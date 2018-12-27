4f414ce741bac49f061fcec1dc0c0c4f3b6db01c
stanislav.goncharov <goncharov.stanslav@gmail.com>
2018-12-28 00:36:54 +0200

    Add some code for playing of media
    Signed-off-by: stanislav.goncharov <goncharov.stanslav@gmail.com>

--- app/src/main/java/com/example/localadmin/mymediaapplication/MainActivity.java
+++ app/src/main/java/com/example/localadmin/mymediaapplication/MainActivity.java
@@ -6,12 +6,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.content.ContentUris;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView myTextView;
     Button myBtnOpen;
     Button myBtnPlay;
 
+    final Uri DATA_URI = ContentUris
+            .withAppendedId(
+                    android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    13359);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void onOpenButtonClick(View view) {
         myTextView.setText("Все ок)");
+
+        Uri myUri = "/"; // initialize Uri here
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setDataSource(getApplicationContext(), myUri);
+        mediaPlayer.prepare();
+        mediaPlayer.start();
     }
 
     public void onPlayButtonClick(View view) {
