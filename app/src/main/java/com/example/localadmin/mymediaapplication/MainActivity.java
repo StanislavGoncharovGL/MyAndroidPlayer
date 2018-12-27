9fbb354ca60608979ec17af5b60f5ff087aefd93
stanislav.goncharov <goncharov.stanslav@gmail.com>
2018-12-27 23:54:06 +0200

    Add onClick buttons handlers
    Signed-off-by: stanislav.goncharov <goncharov.stanslav@gmail.com>

--- app/src/main/java/com/example/localadmin/mymediaapplication/MainActivity.java
+++ app/src/main/java/com/example/localadmin/mymediaapplication/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.localadmin.mymediaapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView myTextView;
+    Button myBtnOpen;
+    Button myBtnPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myTextView = (TextView) findViewById(R.id.textView);
+        myBtnOpen = (Button) findViewById(R.id.open);
+        myBtnPlay = (Button) findViewById(R.id.play);
+    }
+
+    public void onOpenButtonClick(View view) {
+        myTextView.setText("Все ок)");
+    }
+
+    public void onPlayButtonClick(View view) {
+        myTextView.setText("не Все ок)");
     }
 }
