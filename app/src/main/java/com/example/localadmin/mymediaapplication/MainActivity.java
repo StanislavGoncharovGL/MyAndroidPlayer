f6c86d8577cb07fc5264bca456ae38004ddeb4e6
stanislav.goncharov <goncharov.stanslav@gmail.com>
2018-12-31 00:06:15 +0200

    Selected media file
    Signed-off-by: stanislav.goncharov <goncharov.stanslav@gmail.com>

--- app/src/main/java/com/example/localadmin/mymediaapplication/MainActivity.java
+++ app/src/main/java/com/example/localadmin/mymediaapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.localadmin.mymediaapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,33 +18,71 @@ public class MainActivity extends AppCompatActivity {
     TextView myTextView;
     Button myBtnOpen;
     Button myBtnPlay;
+    Uri chosenImageUri;
 
-    final Uri DATA_URI = ContentUris
-            .withAppendedId(
-                    android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                    13359);
+    private static final int REQUEST_PICK_AUDIO = 1;
+
+    /*    final Uri DATA_URI = ContentUris
+                .withAppendedId(
+                        android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                        13359);//*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        chosenImageUri = null;
         myTextView = (TextView) findViewById(R.id.textView);
         myBtnOpen = (Button) findViewById(R.id.open);
         myBtnPlay = (Button) findViewById(R.id.play);
+
+/*        ContentResolver contentResolver = getContentResolver();
+        Uri uri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+        if (cursor == null) {
+            // query failed, handle error.
+        } else if (!cursor.moveToFirst()) {
+            // no media on the device
+        } else {
+            int titleColumn = cursor
+                    .getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = cursor
+                    .getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
+            do {
+                long thisId = cursor.getLong(idColumn);
+                String thisTitle = cursor.getString(titleColumn);
+                // ...process entry...
+            } while (cursor.moveToNext());
+        }//*/
     }
 
     public void onOpenButtonClick(View view) {
-        myTextView.setText("Все ок)");
+        Intent intent = new Intent();
+        intent.setType("audio/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Audio "), REQUEST_PICK_AUDIO);
+    }
 
-        Uri myUri = "/"; // initialize Uri here
-        MediaPlayer mediaPlayer = new MediaPlayer();
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        mediaPlayer.setDataSource(getApplicationContext(), myUri);
-        mediaPlayer.prepare();
-        mediaPlayer.start();
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1: {
+                if (resultCode == RESULT_OK) {
+                    chosenImageUri = data.getData();
+                }
+                break;
+            }
+        }
     }
 
     public void onPlayButtonClick(View view) {
         myTextView.setText("не Все ок)");
+/*        Uri myUri = "/"; // initialize Uri here
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setDataSource(getApplicationContext(), myUri);
+        mediaPlayer.prepare();
+        mediaPlayer.start();//*/
     }
 }
